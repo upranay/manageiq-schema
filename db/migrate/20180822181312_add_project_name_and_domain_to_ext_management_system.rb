a2ec2a008f401b4d3e8ef8fec0405aec4e5d9c90
Aniket Pawar <aniket.pawar@click2cloud.net>
2018-09-06 13:07:32 +0530

    Delete 20180822181312_add_project_name_and_domain_to_ext_management_system.rb

--- db/migrate/20180822181312_add_project_name_and_domain_to_ext_management_system.rb
+++
@@ -1,6 +0,0 @@
-class AddProjectNameAndDomainToExtManagementSystem < ActiveRecord::Migration[5.0]
-  def change
-    add_column :ext_management_systems, :project_name, :string
-    add_column :ext_management_systems, :domain_name, :string
-  end
-end
